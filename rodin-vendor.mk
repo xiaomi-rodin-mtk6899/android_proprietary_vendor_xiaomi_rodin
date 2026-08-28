63eb0e923f861750e72ff47dba64d38df948b40b
klozz <carlosj@klozz.dev>
2026-08-28 11:42:47 +0000

    rodin:sync
    Signed-off-by: klozz <carlosj@klozz.dev>

--- rodin-vendor.mk
+++ rodin-vendor.mk
@@ -1194,14 +1194,12 @@ PRODUCT_PACKAGES += \
     libcodec2_aidl_prebuilt \
     libcodec2_fsr \
     libcodec2_hal_common_prebuilt \
-    libcodec2_hidl_plugin_prebuilt \
     libcodec2_mtk_c2store \
     libcodec2_mtk_vdec \
     libcodec2_mtk_venc \
     libcodec2_soft_ac4dec \
     libcodec2_soft_ddpdec \
     libcodec2_store_dolby \
-    libcodec2_vndk_prebuilt \
     libcodec2_vpp_AIMEMC_plugin \
     libcodec2_vpp_AISR_plugin \
     libcodec2_vpp_fa_plugin \
